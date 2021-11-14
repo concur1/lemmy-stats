217a832103685bfbc943c91257f3525b187f787f
concur1 <87602390+concur1@users.noreply.github.com>
2021-11-14 14:17:42 +0000

    Add files via upload

---
+++ convert_to_csv.py
@@ -0,0 +1,94 @@
+import os
+import json
+import csv
+from zipfile import ZipFile
+
+
+def load_from_json_dir(raw_json_dir):
+    all_json_data = []
+    for file in os.listdir(raw_json_dir):
+        if file.endswith(".json"):
+            with open(f"{raw_json_dir}/{file}", "r") as f:
+                all_json_data.append(json.load(f))
+    return all_json_data
+
+
+def process_json_data(all_json_data):
+    processed_json_data = []
+    for file in all_json_data:
+        for instance in file:
+            if instance["status"] == "Success":
+                row = {"timestamp": instance["timestamp"],
+                     "url": instance["url"],
+                     "status": instance["status"],
+                     "name": instance["json"]["site_view"]["site"]["name"],
+                     "description": instance["json"]["site_view"]["site"]["description"],
+                     "users": instance["json"]["site_view"]["counts"]["users"],
+                     "posts": instance["json"]["site_view"]["counts"]["posts"],
+                     "comments": instance["json"]["site_view"]["counts"]["comments"],
+                     "communities": instance["json"]["site_view"]["counts"]["communities"],
+                     "users_active_day": instance["json"]["site_view"]["counts"]["users_active_day"],
+                     "users_active_week": instance["json"]["site_view"]["counts"]["users_active_week"],
+                     "users_active_month": instance["json"]["site_view"]["counts"]["users_active_month"],
+                     "users": instance["json"]["site_view"]["counts"]["users"],
+                     "users_active_half_year": instance["json"]["site_view"]["counts"]["users_active_half_year"],
+                     "updated": instance["json"]["site_view"]["site"]["updated"],
+                     "linked_federated_instances": instance["json"]["federated_instances"]["linked"],
+                     "allowed_federated_instances": instance["json"]["federated_instances"]["allowed"]
+                       }
+                processed_json_data.append(row)
+    return processed_json_data
+
+
+def write_historical_csv(processed_json_data):
+    for row in processed_json_data:
+        fieldnames = row.keys()
+        if not os.path.isfile(historical_filepath):
+            with open(f"data/historical.csv", "w") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(row)
+        else:
+            with open(f"data/historical.csv", "a") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writerow(row)
+
+
+def zip_json_files(raw_json_dir):
+    for file in os.listdir(raw_json_dir):
+        if file.endswith(".json"):
+            json_file_path = f'{raw_json_dir}/{file}'
+            zip_file_path = f'{raw_json_dir}/hist_json_archive.zip'
+            if not os.path.isfile(zip_file_path):
+                with ZipFile(zip_file_path, 'w') as zipObj2:
+                    zipObj2.write(json_file_path)
+            else:
+                with ZipFile(zip_file_path, 'a') as zipObj2:
+                    zipObj2.write(json_file_path)
+            os.remove(json_file_path)
+
+
+def check_processed_json_for_new_instances(processed_json_data):
+    f = open("known_instances.txt", "r")
+    metadata_instances = set(f.read().splitlines())
+    f.close()
+    all_instances = set()
+    for row in processed_json_data:
+        linked = row["linked_federated_instances"]
+        if linked is not None:
+            all_instances.update(linked)
+        allowed = row["allowed_federated_instances"]
+        if allowed is not None:
+            all_instances.update(allowed)
+    unknown_instances = [f"https://{instance}" for instance in all_instances if f"https://{instance}" not in metadata_instances]
+    print(f"Instances not in known_instances.txt: {unknown_instances}")
+
+
+historical_filepath = "data/historical.csv"
+raw_json_dir = "data/raw_json"
+
+all_json_data = load_from_json_dir(raw_json_dir)
+processed_json_data = process_json_data(all_json_data)
+check_processed_json_for_new_instances(processed_json_data)
+write_historical_csv(processed_json_data)
+zip_json_files(raw_json_dir)
+
---
+++ fetch_and_save.py
@@ -0,0 +1,56 @@
+import json
+import datetime
+import aiohttp
+import asyncio
+
+
+async def fetch(url, session):
+    try:
+        async with session.get(f"{url}/api/v3/site") as response:
+            api_json_text = await response.text()
+    except Exception as e:
+        print(f"{url}/api/v3/site" + " "*(60-len(url)) + "(Failed)")
+        return {"timestamp": str(timestamp), "status": "No Response", "url": url, "Exception": str(e), "json": None}
+    if api_json_text is None:
+        print(f"{url}/api/v3/site" + " " * (60 - len(url)) + "(Failed)")
+        return {"timestamp": str(timestamp), "status": "No API", "url": url, "Exception": None,  "json": None}
+    else:
+        try:
+            api_json = json.loads(api_json_text)
+        except:
+            print(f"{url}/api/v3/site" + " " * (60 - len(url)) + "(Failed)")
+            return {"timestamp": str(timestamp), "status": "Could not parse API", "url": url, "Exception": None, "json": None}
+
+    json_filtered = {}
+    json_filtered['site_view'] = api_json['site_view']
+    json_filtered['online'] = api_json['online']
+    json_filtered['version'] = api_json['version']
+    json_filtered['federated_instances'] = api_json['federated_instances']
+
+    print(f"{url}/api/v3/site" + " " * (60 - len(url)) + "(Success)")
+    return {"timestamp": str(timestamp), "status": "Success", "url": url, "Exception": None, "json": json_filtered}
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for site in sites:
+            tasks.append(asyncio.create_task(fetch(site, session)))
+        response = await asyncio.gather(*tasks)
+    return response
+
+
+timestamp = datetime.datetime.now()
+timestamp_str = str(timestamp)
+f = open("known_instances.txt", "r")
+sites = f.read().splitlines()
+f.close()
+html_pages = asyncio.run(main())
+rows = []
+for page in html_pages:
+    rows.append(page)
+
+with open(f'data/raw_json/{timestamp_str} - lemmy instances stats.json', 'w') as f:
+    json.dump(rows, f)
+
+print(f"runtime: {datetime.datetime.now() - timestamp}")
