8e266ed72e02711d44d5a43388fef8ff3534e7ec
concur1 <ahcyaskx4@relay.firefox.com>
2021-11-30 18:36:23 +0000

    updated script permissions.

--- start_dash_app.sh
+++ start_dash_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
-gunicorn -w 3 index:server &
-watch -n 1200 get_lemmy_data.sh
+gunicorn -w 3 index:server
+
--- start_lemmy_stats.sh
+++ start_lemmy_stats.sh
@@ -1,8 +1,6 @@
+#!/bin/bash
+
 pkill gunicorn
-./start_dash_app.sh &
-interval=3600
-while [[ ! -f /home/22/job_stop.txt ]] ; do
-    now=$(date +%s) # timestamp in seconds
-    sleep $((interval - now % interval))
-    bash ./get_lemmy_data.sh >> logs/fetch.log
-done
+source venv/bin/activate
+gunicorn -w 3 index:server &
+watch -n 1200 get_lemmy_data.sh
