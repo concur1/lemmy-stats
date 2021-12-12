2bfdbb3aa0e6292c4b8d025d62b9893565e43963
concur1 <ahcyaskx4@relay.firefox.com>
2021-12-12 14:40:01 +0000

    Moved xaxis to top for instance comparison.

--- apps/instance_comparison.py
+++ apps/instance_comparison.py
@@ -41,7 +41,7 @@ def update_combined_instances(yaxis_column):
     fig = px.bar(df, y="name", x=yaxis_column, color="name", text="name", orientation='h', height=len(df)*50,
                  template=template)
     fig.update_traces(texttemplate='%{text}')
-    fig = fig.update_layout(yaxis_visible=False, font=font, showlegend=False, margin=dict(l=0, r=0))
+    fig = fig.update_layout(yaxis_visible=False, font=font, showlegend=False, margin=dict(l=0, r=0), xaxis={'side': 'top'})
     return fig
 
 
